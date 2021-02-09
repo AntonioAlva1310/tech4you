6cc9d2a196477b38df43b53b8fcf3fc22610ef05
AntonioAlva1310 <smatechbeat@gmail.com>
2021-02-09 16:14:33 -0600

    adding email as username, adding files for urls and views

--- backend/base/views/user_views.py
+++ backend/base/views/user_views.py
@@ -1,20 +1,21 @@
 from django.shortcuts import render
-from django.http import JsonResponse
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
 
 
-from .products import products
-
 from django.contrib.auth.models import User
-from .models import Product
-from .serializers import ProductSerializer, UserSerializer, UserSerializerWithToken
+
+from base.serializers import UserSerializer, UserSerializerWithToken
 
 
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
-# Create your views here.
+
+from django.contrib.auth.hashers import make_password
+from rest_framework import status
+
+
 
 class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
     def validate(self, attrs):
@@ -31,25 +32,26 @@ class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
 class MyTokenObtainPairView(TokenObtainPairView):
     serializer_class = MyTokenObtainPairSerializer
 
+@api_view(['POST'])
+def registerUser(request):
+    data = request.data
+    print('DATA' , data)
+    try:
+        user = User.objects.create(
+            first_name= data['name'],
+            username = data['email'],
+            email=data['email'],
+            password= make_password(data['password'])
 
-@api_view(['GET'])
-def getRoutes(request):
+        )
 
-    routes = [
-    
-    '/api/products/',
-    '/api/products/create',
-      '/api/products/upload',
-       '/api/products/<id>/reviews',
-        '/api/products/top/',
-         '/api/products/<id>',
-          '/api/products/delete/<id>/',
-           '/api/products/<update>/<id>/',
-    
-    
-    
-    ]
-    return Response(routes)
+        serializer = UserSerializerWithToken(user, many=False)
+        return Response(serializer.data)
+    except:
+        message ={'detail': "User with this email alreadey exists"}
+        return Response(message, status= status.HTTP_400_BAD_REQUEST)
+
+
 
 
 @api_view(['GET'])
@@ -57,7 +59,6 @@ def getRoutes(request):
 def getUserProfile(request):
     user = request.user
     serializer = UserSerializer(user, many=False)
-    print(serializer.data)
     return Response(serializer.data)
 
 @api_view(['GET'])
@@ -65,19 +66,5 @@ def getUserProfile(request):
 def getUsers(request):
     users = User.objects.all()
     serializer = UserSerializer(users, many=True)
-    print(serializer.data)
     return Response(serializer.data)
 
-@api_view(['GET'])
-def getProducts(request):
-    products = Product.objects.all()
-    serializer = ProductSerializer(products, many=True)
-    print(serializer.data)
-    return Response(serializer.data)
-
-
-@api_view(['GET'])
-def getProduct(request, pk):
-    product = Product.objects.get(_id=pk)
-    serializer = ProductSerializer(product, many=False)
-    return Response(serializer.data)
